e0e2887941068b6c705a618710123ca71286134f
dani heredia <daniel.heredia@manomano.com>
2024-09-15 11:27:46 +0200

    POC Kafka producer clicks simulation

---
+++ src/test/java/com/hered7/userclicks/UserClicksProducerTests.java
@@ -0,0 +1,13 @@
+package com.hered7.userclicks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserClicksProducerTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
